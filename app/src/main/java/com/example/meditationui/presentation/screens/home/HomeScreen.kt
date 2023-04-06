301330e415a2a26f298176ee257e67ddcda80d23
kocerenes <eneskocerr41@gmail.com>
2023-04-06 16:12:32 +0300

    Create the GreetingSection and ChipSection design

---
+++ app/src/main/java/com/example/meditationui/presentation/screens/home/HomeScreen.kt
@@ -0,0 +1,96 @@
+package com.example.meditationui.presentation.screens.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.meditationui.presentation.ui.theme.ButtonBlue
+import com.example.meditationui.presentation.ui.theme.DarkerButtonBlue
+import com.example.meditationui.presentation.ui.theme.DeepBlue
+import com.example.meditationui.presentation.ui.theme.TextWhite
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection()
+            ChipSection(chips = listOf("Sweet sleep", "Rock", "Insomnia", "Depression"))
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Enes Koçer"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good Morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "we wish you have a good day.",
+                style = MaterialTheme.typography.body1
+            )
+        }
+
+        Icon(
+            painter = painterResource(id = com.example.meditationui.R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+                    .clickable { selectedChipIndex = it }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(16.dp)
+            ) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+}
