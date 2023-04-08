0a2b7112d6e0170e75db5f4089b40004dd2f548f
kocerenes <eneskocerr41@gmail.com>
2023-04-08 15:12:06 +0300

    Bug fixed

--- app/src/main/java/com/example/meditationui/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/meditationui/presentation/screens/home/HomeScreen.kt
@@ -3,7 +3,18 @@ package com.example.meditationui.presentation.screens.home
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -12,10 +23,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -27,7 +41,24 @@ import androidx.compose.ui.unit.sp
 import com.example.meditationui.R
 import com.example.meditationui.model.BottomMenuContent
 import com.example.meditationui.model.Feature
-import com.example.meditationui.presentation.ui.theme.*
+import com.example.meditationui.presentation.ui.theme.AquaBlue
+import com.example.meditationui.presentation.ui.theme.Beige1
+import com.example.meditationui.presentation.ui.theme.Beige2
+import com.example.meditationui.presentation.ui.theme.Beige3
+import com.example.meditationui.presentation.ui.theme.BlueViolet1
+import com.example.meditationui.presentation.ui.theme.BlueViolet2
+import com.example.meditationui.presentation.ui.theme.BlueViolet3
+import com.example.meditationui.presentation.ui.theme.ButtonBlue
+import com.example.meditationui.presentation.ui.theme.DarkerButtonBlue
+import com.example.meditationui.presentation.ui.theme.DeepBlue
+import com.example.meditationui.presentation.ui.theme.LightGreen1
+import com.example.meditationui.presentation.ui.theme.LightGreen2
+import com.example.meditationui.presentation.ui.theme.LightGreen3
+import com.example.meditationui.presentation.ui.theme.LightRed
+import com.example.meditationui.presentation.ui.theme.OrangeYellow1
+import com.example.meditationui.presentation.ui.theme.OrangeYellow2
+import com.example.meditationui.presentation.ui.theme.OrangeYellow3
+import com.example.meditationui.presentation.ui.theme.TextWhite
 import com.example.meditationui.util.standardQuadFromTo
 
 private val featureList = listOf(
@@ -81,8 +112,9 @@ fun HomeScreen() {
             ChipSection(chips = listOf("Sweet sleep", "Rock", "Insomnia", "Depression"))
             CurrentMeditation()
             FeatureSection(features = featureList)
-            BottomMenu(items = bottomMenuItemList)
         }
+
+        BottomMenu(items = bottomMenuItemList, modifier = Modifier.align(Alignment.BottomCenter))
     }
 }
 
